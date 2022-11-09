4821aa71d530cacfb73862f98478bce828271e5f
Nayana Ramesh <107012@tfcorp.com>
2022-11-09 20:13:54 +0530

    backend

---
+++ Sorting/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Sorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bubble sort = new Bubble();
+            sort.BubbleSort();
+
+            //Selection sort2 = new Selection();
+            //sort2.SelectionSort();
+
+            //Quick sort3 = new Quick();
+            //sort3.QuickSort();
+        }
+    }
+}
